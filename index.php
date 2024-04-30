9d929848896176fb934512289cf967363b2e2043
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-05-01 01:48:46 +0700

    Add dashboard view

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+  // MEMANGGIL HEADER => ('SIDEBAR', 'NAVBAR')
+  include('components/header.php');
+
+  // MEMANGGIL AREA 
+  include('components/canvas.php');
+
+  // MEMANGGIL FOOTER
+  include('components/footer.php');
+?>
