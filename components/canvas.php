668aa085b3b63bf3f17ad83505b5f498cca9a2a9
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-05-02 02:39:55 +0700

    Delete file canvas.php

--- components/canvas.php
+++
@@ -1,8 +0,0 @@
-    <!--  CANVAS -->
-    <div class="container-fluid py-4">
-        <div class="row min-vh-80 h-100">
-          <div class="col-12">
-            Hello World
-              
-          </div>
-        </div>
