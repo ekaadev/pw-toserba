3a6193498e45b2d174c3f4ecceabbef674b91bbe
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-05-02 02:45:32 +0700

    Feat(route): Add a route on the menu

--- components/sidebar.php
+++ components/sidebar.php
@@ -3,7 +3,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="#" target="_blank">\
+      <a class="navbar-brand m-0" href="http://localhost:90/pw-toserba/pages/index.php" target="_self">\
 
         <!-- PLACE IMAGE OR LOGO -->
 
@@ -14,19 +14,27 @@
     <div class="collapse navbar-collapse  w-auto  max-height-vh-100" id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link text-white " href="#">
+          <a class="nav-link text-white " href="http://localhost:90/pw-toserba/pages/index.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">dashboard</i>
             </div>
-            <span class="nav-link-text ms-1">First page</span>
+            <span class="nav-link-text ms-1">Dashboard</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white " href="#">
+          <a class="nav-link text-white" href="http://localhost:90/pw-toserba/pages/purchase.php">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="material-icons opacity-10">table_view</i>
+              <i class="material-icons opacity-10">shopping_cart</i>
             </div>
-            <span class="nav-link-text ms-1">Second Page</span>
+            <span class="nav-link-text ms-1">Purchase</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="http://localhost:90/pw-toserba/pages/inventory.php">
+            <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+              <i class="material-icons opacity-10">inventory</i>
+            </div>
+            <span class="nav-link-text ms-1">Inventory</span>
           </a>
         </li>
       </ul>
