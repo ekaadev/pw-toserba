c0a851dccfab42b0323dd6c5cee8730ca03577eb
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-05-22 17:39:00 +0700

    Style: add sticky to navbar

--- components/navbar.php
+++ components/navbar.php
@@ -1,4 +1,4 @@
-  <nav class="navbar bg-gray-100 navbar-main navbar-expand-lg px-0 mx-4 shadow-none rounded-bottom mb-4" id="navbarBlur" navbar-scroll="true">
+  <nav class="navbar bg-gray-100 navbar-main navbar-expand-lg px-0 mx-4 shadow-sm sticky-top rounded-bottom mb-4" id="navbarBlur" navbar-scroll="true">
         <div class="container-fluid py-1 px-3">
           <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
             <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -21,8 +21,6 @@
                 </a>
               </li>
             </ul> -->
-
-
           </div>
         </div>
   </nav>
