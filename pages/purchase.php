ab849c95eb7bc2c00541e9a579ecc2bfa65b32f5
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-05-30 16:28:31 +0700

    Feat: add view input items on purhase pages

--- pages/purchase.php
+++ pages/purchase.php
@@ -4,11 +4,46 @@
   include('../components/head.php'); 
 ?>
 
+  <div class="container-fluid">
+    <div class="row min-vh-80 h-100 px-5 py-5">
+      <div class="col-12">
+        <p class="fs-2 mb-4">Purchase</p>
+        
+        <!-- input barang -->
+        <div class="row d-flex flex-column rounded-2 bg-white shadow-sm border fs-5">
+          <div class="border-bottom py-3">
+            Input barang
+          </div>
+          <div class="sel-input col-12 d-flex gap-1 pt-3 pb-5 border-bottom">
 
-  <div class="row min-vh-80 h-100 px-5 py-5">
-    <div class="col-12">
-      <span class="">Purchase</span>
-      <div class="form"></div>
+            <!-- baris inputan  -->
+            <div class="col-4">
+              <div>
+                <label for="idBarnag">ID Barang</label>
+              </div>
+              <input type="text" name="idBarang" class="form-control mt-2">
+            </div>
+            <div class="col-4">
+              <div>
+                <label for="kuantitasBarang">Kuantitas</label>
+              </div>
+              <input type="text" name="kuantitasBarang" class="form-control mt-2">
+            </div>
+            <div class="col-4">
+              <div>
+                <label for="jumlahBarang">Jumlah Barang</label>
+              </div>
+              <input type="text" name="jumlahBarang" class="form-control mt-2">
+            </div>
+
+          </div>
+
+          <!-- submit -->
+          <div class="py-2 d-flex justify-content-end ms-2">
+            <input type="button" value="Submit" class="btn btn-primary">
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
