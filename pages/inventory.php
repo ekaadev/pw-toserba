f44103ea533598b4319b8e282d42d6755750d2d0
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-06-20 15:16:07 +0700

    Feat: add view on inventory page

--- pages/inventory.php
+++ pages/inventory.php
@@ -15,8 +15,37 @@
 
       <div class="container-fluid">
         <div class="row min-vh-80 h-100 px-5 py-5">
-          <div class="col-12">
-            <p>Inventory</p>
+          <p class="fs-2 pb-3">Inventory</p>
+          
+          <div class="col-12 rounded-2 bg-white shadow-sm border fs-5 px-5">
+
+            <!-- ROW SEARCH AND ADD -->
+            <form action="" method="get">
+              <div class="py-5 fs-5 d-flex flex-row gap-2">
+                <input type="text" name="search" id="" class="form-control form-control-transparent" placeholder="Search">
+                <button type="submit" class="btn btn-primary" name="search">Search</button>
+                <button type="submit" class="btn btn-secondary" name="add">Add</button>
+              </div>
+            </form>
+
+            <!-- ROW TABLE -->
+            <div class="col-12">
+              <table class="table table-hover">
+                <thead>
+                  <th>tes</th>
+                  <th>test</th>
+                  <th>tdest</th>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>a</td>
+                    <td>a</td>
+                    <td>a</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div> 
+
           </div>
         </div>
       </div>
