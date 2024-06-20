e73ca5742af079837522fe39b5814afd65156680
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-06-20 21:00:58 +0700

    Add page supplier

---
+++ pages/supplier.php
@@ -0,0 +1,29 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php  include('../components/scriptStyle.php'); ?>
+    <title>Supplier</title>
+</head>
+<body class="bg-light">
+    <div id="db-wrapper">
+        <?php include('../components/side.php'); ?>
+
+        <div id="page-content">
+            <?php include('../components/nav.php'); ?>
+
+            <div class="container-fluid">
+
+                <div class="row min-vh-80 h-100 px-5 py-5">
+                    <p class="fs-2 pb-3">Supplier</p>
+                </div>
+
+
+            </div>
+
+        </div>
+    </div>
+
+
+</body>
+</html>
