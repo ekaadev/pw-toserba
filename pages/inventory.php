584af6109af279e6f35e5e1b0151c6f1a90852af
Yohanes Eka Putra <ekaadev9@gmail.com>
2024-06-21 08:43:15 +0700

    Feat: add feature live search

--- pages/inventory.php
+++ pages/inventory.php
@@ -3,6 +3,10 @@
 
   session_start();
 
+  if (isset($_GET['refresh'])) {
+    header('Location: inventory.php');
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,79 +30,135 @@
             
             
             <!-- ROW SEARCH AND ADD -->
-            <form action="" method="get">
+            <form action="inventory.php" method="get">
               <div class="py-5 fs-5 d-flex flex-row gap-2">
-                <input type="text" name="search" id="" class="form-control form-control-transparent" placeholder="Search">
-                <button type="submit" class="btn btn-primary" name="search">Search</button>
+                <input type="text" name="key" id="key" class="form-control form-control-transparent" placeholder="Search">
+                <button type="submit" class="btn btn-primary" name="search" id="cari">Search</button>
                 <button type="submit" class="btn btn-secondary" name="add">Add</button>
+                <button type="submit" class="btn btn-success" name="refresh">Refresh</button>
               </div>
             </form>
 
             <!-- ROW TABLE -->
-            <table class="table table-hover">
-              <thead>
-                <th>ID Barang</th>
-                <th>ID Kategori</th>
-                <th>Nama</th>
-                <th>Stok</th>
-                <th>Harga Beli</th>
-                <th>Harga Jual</th>
-                <th>Action</th>
-              </thead>
-              <tbody>
-                <?php 
-                  try {
+            <div id="content">
+              
+              <table class="table table-hover">
+                <thead>
+                  <th>ID Barang</th>
+                  <th>ID Kategori</th>
+                  <th>Nama</th>
+                  <th>Stok</th>
+                  <th>Harga Beli</th>
+                  <th>Harga Jual</th>
+                  <th>Action</th>
+                </thead>
+                <tbody>
+                  <?php 
                     
-                    // konek database
-                    $conn = Connection::getConnection();
-
-                    // fetch data
-                    $sql = "SELECT id_barang, id_kategori, nama, stok, harga_beli, harga_jual FROM barang";
-
-                    $result = $conn->query($sql);
-
-                    // inisiasi variabel
-                    $id_barang = null;
-                    $id_kategori = null;
-                    $nama = null;
-                    $stok = null;
-                    $harga_beli = null;
-                    $harga_jual = null;
-
-                    // fetch data
-                    foreach($result as $value) {
-                      $id_barang = $value['id_barang'];
-                      $id_kategori = $value['id_kategori'];
-                      $nama = $value['nama'];
-                      $stok = $value['stok'];
-                      $harga_beli = $value['harga_beli'];
-                      $harga_jual = $value['harga_jual'];
-
-
-                      echo "<tr>";
-                      echo "<td>$id_barang</td>";
-                      echo "<td>$id_kategori</td>";
-                      echo "<td>$nama</td>";
-                      echo "<td>$stok</td>";
-                      echo "<td>$harga_beli</td>";
-                      echo "<td>$harga_jual</td>";
-                      echo "<td>
-                              <button class=\"btn btn-primary \" type=\"submit\" name=\"edit\" value=\"$id_barang\">Edit</button>
-                              <button class=\"btn btn-warning \" type=\"submit\" name=\"delete\" value=\"$id_barang\">Delete</button>
-                            </td>";
-                      echo "</tr>";
-                    }
-                    
-            
-                    $conn = null;
-                  } catch(PDOException $e) {
-                    echo "Error : " . $e->getMessage();
-                  }
-                
-                ?>
-              </tbody>
-            </table>
+                    try {
+                      
+                      // connect database
+                      $conn = Connection::getConnection();
+                      
+                      // fetch data
+                      $sql = "SELECT id_barang, id_kategori, nama, stok, harga_beli, harga_jual FROM barang";
+                      
+                      $result = $conn->query($sql);
+                      
+                      // inisiasi variabel
+                      $id_barang = null;
+                      $id_kategori = null;
+                      $nama = null;
+                      $stok = null;
+                      $harga_beli = null;
+                      $harga_jual = null;
+                      
+                      if (!isset($_GET['search'])) {
+                      
+                        // fetch data
+                        foreach($result as $value) {
+                          $id_barang = $value['id_barang'];
+                          $id_kategori = $value['id_kategori'];
+                          $nama = $value['nama'];
+                          $stok = $value['stok'];
+                          $harga_beli = $value['harga_beli'];
+                          $harga_jual = $value['harga_jual'];
   
+  
+                          echo "<tr>";
+                          echo "<td>$id_barang</td>";
+                          echo "<td>$id_kategori</td>";
+                          echo "<td>$nama</td>";
+                          echo "<td>$stok</td>";
+                          echo "<td>$harga_beli</td>";
+                          echo "<td>$harga_jual</td>";
+                          echo "<td>
+                                  <form action=\"\" method=\"get\">
+                                  <button class=\"btn btn-primary \" type=\"submit\" name=\"edit\" value=\"$id_barang\">Edit</button>
+                                  <button class=\"btn btn-danger \" type=\"submit\" name=\"delete\" value=\"$id_barang\">Delete</button>
+                                  </form>
+                                </td>";
+                          echo "</tr>";
+                        }
+                        
+                
+                        $conn = null;
+
+                      } if(isset($_GET['search'])) {
+
+                        $keyword = $_GET['key'];
+                        $conn = Connection::getConnection();
+                      
+                        // fetch data
+                        $sql = "SELECT id_barang, id_kategori, nama, stok, harga_beli, harga_jual FROM barang WHERE nama LIKE '%$keyword%'";
+                        
+                        $result = $conn->query($sql);
+                        
+                        // inisiasi variabel
+                        $id_barang = null;
+                        $id_kategori = null;
+                        $nama = null;
+                        $stok = null;
+                        $harga_beli = null;
+                        $harga_jual = null;
+
+                        foreach($result as $value) {
+                          $id_barang = $value['id_barang'];
+                          $id_kategori = $value['id_kategori'];
+                          $nama = $value['nama'];
+                          $stok = $value['stok'];
+                          $harga_beli = $value['harga_beli'];
+                          $harga_jual = $value['harga_jual'];
+  
+  
+                          echo "<tr>";
+                          echo "<td>$id_barang</td>";
+                          echo "<td>$id_kategori</td>";
+                          echo "<td>$nama</td>";
+                          echo "<td>$stok</td>";
+                          echo "<td>$harga_beli</td>";
+                          echo "<td>$harga_jual</td>";
+                          echo "<td>
+                                  <form action=\"\" method=\"get\">
+                                  <button class=\"btn btn-primary \" type=\"submit\" name=\"edit\" value=\"$id_barang\">Edit</button>
+                                  <button class=\"btn btn-danger \" type=\"submit\" name=\"delete\" value=\"$id_barang\">Delete</button>
+                                  </form>
+                                </td>";
+                          echo "</tr>";
+                        }
+                        
+
+                      }
+
+
+                    } catch(PDOException $e) {
+                      echo "Error : " . $e->getMessage();
+                    }
+                  
+                  ?>
+                </tbody>
+              </table>
+            </div>
 
           </div>
         </div>
@@ -110,5 +170,6 @@
   </div>
 
   <?php include('../components/scripts.php') ?>
+  <script src="../assets/js/script.js"></script>
 </body>
 </html>
